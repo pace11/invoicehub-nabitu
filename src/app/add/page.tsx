aa379d11e776a278d06d1089ab7db6de47ff7414
pace11 <ryanjoker87@gmail.com>
2025-02-08 12:12:49 +0700

    feat: install MUI Next.js and adjustment base layout

---
+++ src/app/add/page.tsx
@@ -0,0 +1,9 @@
+import { Typography } from '@mui/material'
+
+export default function Add() {
+  return (
+    <>
+      <Typography>Add Invoice</Typography>
+    </>
+  )
+}
---
+++ src/app/list/page.tsx
@@ -0,0 +1,9 @@
+import { Typography } from '@mui/material'
+
+export default function List() {
+  return (
+    <>
+      <Typography>List Invoices</Typography>
+    </>
+  )
+}
